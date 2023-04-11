f1171ffdbff6a61d4e5b819b8d8506c9bbe77eb6
Dmitry Kozak <dimakaza4ka@gmail.com>
2023-04-11 15:42:00 +0300

    Working after StableDiff, making the battleSystem

--- Assets/Scripts/BattleSystem/CardDeck.cs
+++ Assets/Scripts/BattleSystem/CardDeck.cs
@@ -6,8 +6,24 @@ using UnityEngine;
 [Serializable]
 public class CardDeck 
 {
-  
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private int attack;
+    [SerializeField]
+    private int cost;
+    
+
+    public CardDeck(string name,int attack,int cost)
+    {
+        this.name = name;
+        this.attack = attack;
+        this.cost = cost;
+    }
 
 
+    public string getName() { return name; }
+    public int getAttack() { return attack; }
+    public int getCost() { return cost; }
 
 }
